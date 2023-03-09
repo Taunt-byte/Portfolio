4537a440cfe92e30a74869dcdf4686e8d065473f
Lucas Neves <lucasluan94@hotmail.com>
2023-03-09 14:00:02 -0300

    LICENSE

--- app/.next/server/flight-server-css-manifest.js
+++ app/.next/server/flight-server-css-manifest.js
@@ -1,10 +1,3 @@
 self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {
-    "D:\\Projetos\\Portfolio\\app\\src\\app\\page": [
-      "D:\\Projetos\\Portfolio\\app\\src\\app\\globals.css"
-    ]
-  },
-  "D:\\Projetos\\Portfolio\\app\\src\\app\\layout.tsx": [
-    "D:\\Projetos\\Portfolio\\app\\src\\app\\globals.css"
-  ]
+  "__entry_css_mods__": {}
 }
